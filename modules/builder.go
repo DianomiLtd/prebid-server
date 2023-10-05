fa376ad6bb9204dd69a1bad244827b565e0650a2
Michael Stevens <michael.stevens@dianomi.com>
2023-10-05 16:35:53 +0100

    Add new seat module as submodule

--- modules/builder.go
+++ modules/builder.go
@@ -1,6 +1,7 @@
 package modules
 
 import (
+	dianomiPrebid_seat_module "github.com/prebid/prebid-server/modules/dianomi/prebid_seat_module"
 	prebidOrtb2blocking "github.com/prebid/prebid-server/modules/prebid/ortb2blocking"
 )
 
@@ -8,6 +9,9 @@ import (
 // vendor and module names are chosen based on the module directory name
 func builders() ModuleBuilders {
 	return ModuleBuilders{
+		"dianomi": {
+			"prebid_seat_module": dianomiPrebid_seat_module.Builder,
+		},
 		"prebid": {
 			"ortb2blocking": prebidOrtb2blocking.Builder,
 		},
